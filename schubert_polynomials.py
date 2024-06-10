946722d1206a485d13820bb99b142854303800e4
George H. Seelinger <ghseeli@gmail.com>
2024-06-10 12:28:37 -0400

    Added quantum Grothendieck code. Let's see if it works...

--- schubert_polynomials.py
+++ schubert_polynomials.py
@@ -95,6 +95,8 @@ def double_schubert_poly(w):
     longest_word = Permutation(range(n,0,-1))
     return divided_difference_w(w.inverse()*longest_word, base_poly)
 
+## Grothendieck polynomials
+
 def pi_divided_difference(i, poly, alphabet='x'):
     r"""
     Apply the operator `\pi_i(f) = \partial_i((1-x_{i+1})f)` to ``poly``.
@@ -146,6 +148,8 @@ def double_grothendieck_poly(w):
     longest_word = Permutation(range(n,0,-1))
     return poly_ring(pi_divided_difference_w(w.inverse()*longest_word, base_poly))
 
+## Quantum Schubert polynomials
+
 def e_i_m(i, m, ambient_vars, br=QQ, start=1):
     var_string = reduce(lambda a,b: a+','+b, ['x'+str(j) for j in range(start,start+ambient_vars)])
     ambient_ring = br[var_string]
@@ -172,10 +176,10 @@ def Schubert_in_e(perm, base_ring=QQ):
 
 def generate_quantum_polynomial_ring(br, num_vars, x_pref='x', start=1):
     r"""
-    Return the polynomial ring with ``num_vars`` x-variables and ``num_vars``-1 q-variables.
+    Return the polynomial ring with ``num_vars`` x-variables and ``num_vars`` q-variables.
     """
-    xq_vars = ['x'+str(j+start) for j in range(num_vars)]+['q'+str(j+start) for j in range(num_vars-1)] 
-    return PolynomialRing(br, xq_vars, 2*num_vars-1)
+    xq_vars = ['x'+str(j+start) for j in range(num_vars)]+['q'+str(j+start) for j in range(num_vars)] 
+    return PolynomialRing(br, xq_vars, 2*num_vars)
 
 def quantum_e_i_m(i,m,ambient_vars,br=QQ, start=1):
     r"""
@@ -200,3 +204,86 @@ def quantum_e_basis(deg, l, br=QQ, start=1):
     elms = [quantum_e_seq(seq,br,start) for seq in IntegerVectors(deg,length=l)]
     return [elm for elm in elms if elm != 0]
 
+def quantum_Schubert(perm, base_ring=QQ, start=1):
+    r"""
+    Return the quantum Schubert polynomial associated to permutation ``perm`` from the corresponding paper by Fomin-Gelfand-Postnikov.
+    """
+    schub_in_e = Schubert_in_e(perm, base_ring)
+    poly_in_quantum_E = sum([coeff*elm for (elm,coeff) in zip(quantum_e_basis(d, len(perm)-1, br=base_ring, start=start),coeffs_in_e)])
+    return poly_in_quantum_E
+
+## Quantum Grothendieck polynomials
+
+def quantum_F_p_k(p, k, ambient_vars, br=QQ, var_fam='x', start=1):
+    r"""
+    Return the polynomial `F_p^k` from Equation (3.1) in Lenart-Maeno.
+    """
+    assert 0 <= p
+    assert k <= ambient_vars
+    ambient_ring = generate_quantum_polynomial_ring(br, ambient_vars, x_pref, start)
+    if p > k:
+        return ambient_ring.zero()
+    if p==0:
+        return ambient_ring.one()
+    Is = Subsets(range(start,start+k),p)
+    return ambient_ring.zero() + sum(prod(1-ambient_ring(var_fam+str(i)) for i in I)*prod(1-ambient_ring('q'+str(i)) for i in I if i+1 not in I) for I in Is)
+
+def quantum_F_p_k_bar(p, k, ambient_vars, br=QQ, start=1):
+    r"""
+    Return the `\overline{F}_p^k` polynomial from Lenart-Maeno, given by specializing `q_k = 0` in `F_p^k`.
+    """
+    F_p_k = quantum_F_p_k(p,k,ambient_vars,br=br,start=1)
+    par = parent(F_p_k)
+    return F_p_k.subs({par('q'+str(k-1+start)):par.zero()})
+
+def quantum_F_p_k_tilde(p, k, ambient_vars, br=QQ, start=1):
+    r"""
+    Compute the `\tilde{F}_p^k` polynomial from equation (3.2) in Lenart-Maeno.
+    """
+    assert 0 <= p
+    assert p <= k
+    assert k <= ambient_vars
+    ambient_ring = generate_quantum_polynomial_ring(br, ambient_vars, start=start)
+    if p==0:
+        return ambient_ring.one()
+    Is = Subsets(range(start,start+k),p)
+    return ambient_ring.zero() + sum(prod(ambient_ring('x'+str(i)) for i in I)*prod(1-ambient_ring('q'+str(i-1)) for i in I-Set([0]) if i-1 not in I) for I in Is)
+
+def quantum_E_p_k_hat(p,k,ambient_vars,br=QQ,start=1):
+    assert p >= 0
+    return sum((-1)**(i)*binomial(k-i,p-i)*quantum_F_p_k(i,k,ambient_vars,br=br,start=start) for i in range(p+1))
+
+def quantum_E_hat_seq(seq, br=QQ, start=1):
+    m = len(seq)
+    return prod(quantum_E_p_k_hat(seq[i],i+1,m,br,start) for i in range(m))
+
+def quantum_E_hat_basis(deg, l, br=QQ, start=1):
+    elms = [quantum_E_hat_seq(seq, br, start) for d in reversed(range(deg+1)) for seq in IntegerVectors(d,length=l)]
+    return [elm for elm in elms if elm != 0]
+
+def inhomog_e_basis(top_deg, l, br=QQ, start=1):
+    return [elm for d in reversed(range(top_deg+1)) for elm in e_basis(d, l, br=br, start=start)]
+
+def quantum_Grothendieck(perm, base_ring=QQ):
+    r"""
+    Compute the quantum Grothendieck polynomial associated to ``perm`` as in Lenart-Maeno.
+
+    EXAMPLES::
+
+        sage: quantum_Grothendieck([1,2,3])
+        1
+        sage: quantum_Grothendieck([2,1,3])
+        -x1*q1 + x1 + q1
+        sage: quantum_Grothendieck([3,2,1]) == quantum_Grothendieck([2,1,3])*quantum_Grothendieck([2,3,1])
+        True
+    """
+    l = len(perm)
+    poly = grothendieck_poly(perm)
+    poly_par = parent(poly)
+    if poly == poly_par.one():
+        return quantum_E_hat_seq([0]*l)
+    d = poly.degree()
+    basis = inhomog_e_basis(d, len(perm)-1, br=base_ring)
+    coeffs_in_e = solve_polynomial_in_terms_of_basis(poly, basis, base_ring)
+    poly_in_quantum_E = sum([coeff*elm for (elm,coeff) in zip(quantum_E_hat_basis(d, len(perm)-1, br=base_ring),coeffs_in_e)])
+    return poly_in_quantum_E
