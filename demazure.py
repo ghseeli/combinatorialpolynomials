2661c4abfdb9f44ed1a88e239e4f87cfdf778b65
George H. Seelinger <ghseeli@gmail.com>
2025-12-19 14:57:03 -0500

    Added some optimizations to demazure.py and expanding into keys.
    Also, I added the key polynomial part function for Laurent
    polynomials, but it is not as safe as I would like yet.
    In particular, I think it will not handle flat polynomials with
    multiple alphabets of variables well.

--- demazure.py
+++ demazure.py
@@ -6,9 +6,12 @@
 #                  https://www.gnu.org/licenses/
 # ***************************************************************************
 
-from sage.all import cartesian_product, MixedIntegerLinearProgram, parent, Permutation, Permutations, prod, QQ, Rationals, RootSystem, Word
-from polynomial_utils import generate_laurent_polynomial_ring, invert_variables_in_flat_polynomial, separate_polynomial_generators, specialize_flat_polynomial_variables
+from functools import cache
+from sage.all import cartesian_product, MixedIntegerLinearProgram, parent, Partition, Permutation, Permutations, prod, QQ, Rationals, RootSystem, Word
+from polynomial_utils import generate_laurent_polynomial_ring, invert_variables_in_flat_polynomial, polynomial_by_degree, separate_polynomial_generators, specialize_flat_polynomial_variables
 from schubert_polynomials import s_i, s_i_on_polynomial, divided_difference, divided_difference_on_polynomial, _iterate_operators_from_reduced_word
+
+## (Isobaric) Demazure operators, key polynomials, atom polynomials
 
 def demazure_pi_i(i, f, alphabet='x'):
     r"""
@@ -68,6 +71,14 @@ def demazure_pi_i_on_polynomial(i, poly, alphabet='x'):
 def demazure_pi_w_on_polynomial(w, poly, alphabet='x'):
     return _iterate_operators_from_reduced_word(demazure_pi_i_on_polynomial, w, poly, alphabet=alphabet)
    
+@cache
+def _integer_vector_to_dominant_weight(alpha):
+    return tuple(sorted(alpha, reverse=True))
+
+@cache
+def _integer_vector_to_orbit_perm(alpha):
+    return Word([-a for a in alpha]).standard_permutation().inverse()
+
 def key_polynomial(alph, alphabet='x', ambient_ring=None):
     r"""
     Return the key polynomial or Demazure character indexed by integer vector ``alph``.
@@ -84,15 +95,24 @@ def key_polynomial(alph, alphabet='x', ambient_ring=None):
         x1^2*x2 + x1*x2^2
         sage: key_polynomial([-2,-1])
         x1^-1*x2^-2 + x1^-2*x2^-1
+        sage: key_polynomial([1,2], alphabet='y')
+        y1^2*y2 + y1*y2^2
     """
     if not ambient_ring:
         ambient_ring = generate_laurent_polynomial_ring(Rationals(), len(alph), alphabet)
-    w = Word([-a for a in alph]).standard_permutation().inverse()
-    sorted_alph = list(reversed(sorted(alph)))
-    monomial = prod([ambient_ring(alphabet+str(i+1))**(sorted_alph[i]) for i in range(len(alph))])
-    return demazure_pi_w_on_polynomial(w, monomial, alphabet)
+    alph = list(alph)
+    if all(alph[i] >= alph[i+1] for i in range(len(alph)-1)):
+        mon = prod(ambient_ring(alphabet+str(i+1))**alph[i] for i in range(len(alph)))
+        return mon
+    for i in range(len(alph)-1):
+        if alph[i] < alph[i+1]:
+            si_alph = alph[:i] + [alph[i+1],alph[i]] + alph[i+2:]
+            return demazure_pi_i_on_polynomial(i+1, key_polynomial(si_alph, alphabet=alphabet, ambient_ring=ambient_ring), alphabet)
 
 def demazure_pi_i_hat(i, poly, alphabet='x'):
+    r"""
+    The operator `\hat{\pi}_i` is defined as `\pi_i-1` for `\pi_i` the (isobaric) Demazure operator. 
+    """
     return demazure_pi_i(i, poly, alphabet)-poly
 
 def demazure_pi_w_hat(w, poly, alphabet='x'):
@@ -112,34 +132,48 @@ def atom_polynomial(alph, alphabet='x', ambient_ring=None):
 
         sage: atom_polynomial([3,2,1])
         x1^3*x2^2*x3
+        sage: atom_polynomial([2,1,3])
+        x1^2*x2^2*x3^2 + x1^2*x2*x3^3
         sage: atom_polynomial([-3,0,0])
         x1^-1*x3^-2 + x1^-1*x2^-1*x3^-1 + x1^-2*x3^-1 + x1^-1*x2^-2 + x1^-2*x2^-1 + x1^-3
     """
     if not ambient_ring:
         ambient_ring = generate_laurent_polynomial_ring(Rationals(), len(alph), alphabet)
-    w = Word([-a for a in alph]).standard_permutation().inverse()
-    sorted_alph = list(reversed(sorted(alph)))
-    monomial = prod([ambient_ring(alphabet+str(i+1))**(sorted_alph[i]) for i in range(len(alph))])
-    return ambient_ring(demazure_pi_w_hat(w, monomial, alphabet))
+    alph = list(alph)
+    if all(alph[i] >= alph[i+1] for i in range(len(alph)-1)):
+        mon = prod(ambient_ring(alphabet+str(i+1))**alph[i] for i in range(len(alph)))
+        return mon
+    for i in range(len(alph)-1):
+        if alph[i] < alph[i+1]:
+            si_alph = alph[:i] + [alph[i+1],alph[i]] + alph[i+2:]
+            return demazure_pi_i_hat_on_polynomial(i+1, atom_polynomial(si_alph, alphabet=alphabet, ambient_ring=ambient_ring), alphabet)
 
-def _setup_demazure_inner_prod_milp_on_monomial_exponents(alpha, beta):
+def _setup_demazure_inner_prod_milp_on_monomial_exponent(gamma):
+    r"""
+    Convienience function to setup a system of inequalities where the number of integer point solutions are the solution
+    to the constant term of `x^{\gamma} \prod_{i<j} (1-x_i/x_j)`.
+    """
     P = MixedIntegerLinearProgram()
     x = P.new_variable(integer=True, nonnegative=True)
-    n = len(beta)-1
+    n = len(gamma)-1
     RS = RootSystem(['A',n])
     ZZnp1 = RS.ambient_space()
     pos_roots = [ZZnp1(r) for r in RS.root_lattice().positive_roots()]
-    for k in range(len(beta)):
-        P.add_constraint(sum(x[i]*pos_roots[i][k] for i in range(len(pos_roots))) + beta[k]+alpha[k] == 0)
+    for k in range(len(gamma)):
+        P.add_constraint(sum(x[i]*pos_roots[i][k] for i in range(len(pos_roots))) + gamma[k] == 0)
     for i in range(len(pos_roots)):
         P.add_constraint(x[i] <= 1)
     return P
 
 def _demazure_inner_prod_on_monomial_exponents(alpha, beta):
+    r"""
+    Convenience function to compute the Demazure inner product on `\langle x^\alpha, x^\beta \rangle`. 
+    """
     if len(alpha) != len(beta):
         alpha = list(alpha) + [0]*(len(beta)-len(alpha))
         beta = list(beta) + [0]*(len(alpha)-len(beta))
-    milp = _setup_demazure_inner_prod_milp_on_monomial_exponents(alpha,beta) 
+    gamma = [alpha[k] + beta[k] for k in range(len(beta))]
+    milp = _setup_demazure_inner_prod_milp_on_monomial_exponent(gamma) 
     return sum((-1)**sum(soln) for soln in milp.polyhedron(base_ring=QQ, backend='normaliz').integral_points())
 
 def demazure_inner_prod(f, g, alphabet='x'):
@@ -172,6 +206,8 @@ def demazure_inner_prod(f, g, alphabet='x'):
     f_coeff_mons = separate_polynomial_generators(fxx, f)
     g_coeff_mons = separate_polynomial_generators(gxx, g)
     return sum(coeff1*coeff2*_demazure_inner_prod_on_monomial_exponents(mon1.exponents()[0], mon2.exponents()[0]) for ((coeff1,mon1),(coeff2,mon2)) in cartesian_product([f_coeff_mons,g_coeff_mons]))
+
+## Demazure-Lusztig operators
 
 def demazure_lusztig_i(i, f, alphabet='x', v=None, convention='hhl'):
     r"""
@@ -252,7 +288,7 @@ def demazure_lusztig_i_inverse_on_polynomial(i, poly, alphabet='x', v=None, conv
     if not v:
         par = parent(poly)
         v = par('t')
-    return v**(-1)*(demazure_lusztig_i(i, poly, alphabet=alphabet, v=v, convention=convention) + (1-v)*poly)
+    return v**(-1)*(demazure_lusztig_i_on_polynomial(i, poly, alphabet=alphabet, v=v, convention=convention) + (1-v)*poly)
 
 def demazure_lusztig_w_on_polynomial(w, poly, alphabet='x', v=None, convention='hhl', offset=0):
     r"""
@@ -275,6 +311,116 @@ def hecke_symmetrize_over_perms(perms, poly, alphabet='x', v=None, convention='h
     if not perm_coeff_dict:
         perm_coeff_dict = {perm:br.one() for perm in perms}
     return sum(perm_coeff_dict[perm]*demazure_lusztig_w_on_polynomial(perm, poly, alphabet=alphabet, v=v, convention=convention) for perm in perms)
+
+def dominance_bruhat_partial_order_le(alpha, beta):
+    r"""
+    Given integer vectors ``alpha`` and ``beta``, return ``True`` if ``alpha`` is `\leq` ``beta`` in
+    the partial order given by first checking if `\alpha_+ < \beta_+` in dominance order when `\alpha_+ \neq \beta_+`,
+    and otherwise checking if `\alpha \leq \beta` in the Bruhat order on `S_l \alpha_+`.
+
+    Note, for now, this is simply used to check properties of various polynomials.
+
+    EXAMPLES::
+
+        sage: dominance_bruhat_partial_order_le([2,1],[2,1])
+        True
+        sage: dominance_bruhat_partial_order_le([2,1],[1,2])
+        True
+        sage: dominance_bruhat_partial_order_le([1,2],[2,1])
+        False
+        sage: dominance_bruhat_partial_order_le([1,1,1],[1,2])
+        True
+
+    In this dominance order, all the nonzero coefficients in a Demazure character should be below the index in dominance order::
+
+        sage: all([all(dominance_bruhat_partial_order_le(beta,alph) for beta in key_polynomial(list(alph)).exponents()) for alph in IntegerVectors(3,3)])
+        True
+        sage: all([all(dominance_bruhat_partial_order_le(beta,alph) for beta in key_polynomial(list(alph)).exponents()) for alph in IntegerVectors(2,3)])
+        True
+
+    """
+    alpha = tuple(alpha)
+    beta = tuple(beta)
+    lam = Partition(_integer_vector_to_dominant_weight(alpha))
+    mu = Partition(_integer_vector_to_dominant_weight(beta))
+    if mu != lam:
+        return mu.dominates(lam) 
+    wa = _integer_vector_to_orbit_perm(alpha)
+    wb = _integer_vector_to_orbit_perm(beta)
+    return wa.bruhat_le(wb)
+    
+def key_expansion_of_laurent_polynomial(f, alphabet='x'):
+    r"""
+    Given a Laurent polynomial ``f``, give its expansion into key_polynomials.
+
+    This makes use of the fact that the exponents of the monomials appearing in a key polynomial
+    are less than the index in reverse-lex order.
+
+    EXAMPLES::
+
+        sage: key_expansion_of_laurent_polynomial(key_polynomial([2,0,1]))
+        [((2, 0, 1), 1)]
+        sage: key_expansion_of_laurent_polynomial(key_polynomial([-2,0,1]))
+        [((-2, 0, 1), 1)]
+        sage: key_expansion_of_laurent_polynomial(key_polynomial([2,0,1])+3*key_polynomial([0,2,1])-key_polynomial([0,3,0]))
+        [((0, 2, 1), 3), ((2, 0, 1), 1), ((0, 3, 0), -1)]
+        sage: all(key_polynomial(alph) == sum([coeff*key_polynomial(supp) for (supp,coeff) in key_expansion_of_laurent_polynomial(key_polynomial(alph))]) for alph in IntegerVectors(4,4))
+        True
+        sage: A = generate_laurent_polynomial_ring(QQ, 2)
+        sage: xx = A.gens()
+        sage: key_expansion_of_laurent_polynomial(xx[0]**(-1)*xx[1])
+        [((-1, 1), 1), ((0, 0), -1), ((1, -1), -1)]
+    """
+    def revlex_key(alpha):
+        return tuple(reversed(alpha))
+    par = parent(f)
+    g = par(f)
+    xx = [x for x in par.gens() if str(x)[0] == alphabet]
+    result = []
+    while g != 0:
+        alpha = max(g.exponents(), key=revlex_key)
+        coeff = g.monomial_coefficient(prod(xx[i]**alpha[i] for i in range(len(alpha))))
+        result.append((tuple(alpha), coeff))
+        g -= coeff * key_polynomial(alpha)
+    return result
+
+def key_expansion_polynomial_part_of_laurent_polynomial(f, alphabet='x'):
+    r"""
+    Given a homogeneous Laurent polynomial ``f`` expanded in monomials, return the Demazure character polynomial part.
+
+    Note, the polynomial part of a Laurent polynomial is given by expanding the polynomial into Demazure
+    characters and setting the non-polynomial ones to zero.
+
+    Equivalently, one can compute the polynomial part of `f` by the formula
+    `\langle f(z), \Omega[\sum_{i \{leq j\} x_i/z_j}] \rangle_0` for the inner product being the
+    Demazure inner product.
+
+    EXAMPLES::
+
+    
+        sage: A = generate_laurent_polynomial_ring(QQ, 2)
+        sage: xx = A.gens()
+        sage: key_expansion_polynomial_part_of_laurent_polynomial(xx[0]**(-1)*xx[1])
+        [((0, 0), -1)]
+    """
+    key_exp = key_expansion_of_laurent_polynomial(f, alphabet=alphabet)
+    return [(supp,coeff) for (supp,coeff) in key_exp if all(supp[i] >= 0 for i in range(len(supp)))]
+    
+def polynomial_part_of_laurent_polynomial(f, alphabet='x'):
+    r"""
+    Given a Laurent polynomial ``f``, return the Demazure character polynomial part of ``f`` as a polynomial.
+
+    EXAMPLES::
+
+        sage: A = generate_laurent_polynomial_ring(QQ, 2)
+        sage: xx = A.gens()
+        sage: polynomial_part_of_laurent_polynomial(xx[0]**(-1)*xx[1])
+        -1
+    """
+    key_poly_part_exp = key_expansion_polynomial_part_of_laurent_polynomial(f, alphabet=alphabet)
+    return sum(coeff*key_polynomial(supp) for (supp,coeff) in key_poly_part_exp)
+
+## Nonsymmetric Hall-Littlewood polynomials
 
 def nonsymmetric_hall_littlewood_E(alph, twist=None, v=None, alphabet='x', ambient_ring=None):
     r"""
@@ -313,6 +459,14 @@ def nonsymmetric_hall_littlewood_E(alph, twist=None, v=None, alphabet='x', ambie
         sage: A = generate_multi_laurent_polynomial_ring(QQ, 3, pre_extra_vars=['t'])
         sage: nonsymmetric_hall_littlewood_E([1,0,1],alphabet='y',ambient_ring=A)*nonsymmetric_hall_littlewood_E([0,1,0],alphabet='x',ambient_ring=A)
         x1*y1*y2 + x2*y1*y2 + x1*y1*y3 + x2*y1*y3 - 2*t^-1*x1*y1*y2 - t^-1*x2*y1*y2 - t^-1*x1*y1*y3 + t^-2*x1*y1*y2
+        sage: A = generate_laurent_polynomial_ring(Frac(QQ['t']), 3)
+        sage: nonsymmetric_hall_littlewood_E([1,0,1],ambient_ring=A)
+        ((t - 1)/t)*x1*x2 + x1*x3
+        sage: K = Frac(QQ['q,t'])
+        sage: B = generate_laurent_polynomial_ring(K, 3)
+        sage: q = K('q')
+        sage: nonsymmetric_hall_littlewood_E([1,0,1],v=q,ambient_ring=B)
+        ((q - 1)/q)*x1*x2 + x1*x3
 
     The nonsymmetric Hall-Littlewood `E(x;t^{-1})` polynomials specialize to key polynomials (or Demazure characters) at `t = 0`.
 
